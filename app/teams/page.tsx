5466b3cb15bb22cc257f24788881cb85f58e251a
LeoFuna <leo.funa@gmail.com>
2024-06-24 15:08:48 -0300

    fix: suspense on teams page

--- app/teams/page.tsx
+++ app/teams/page.tsx
@@ -1,9 +1,12 @@
 import Content from '@/components/teams/Content';
+import { Suspense } from 'react';
 
 export default function TeamsPage() {
   return (
     <main className="flex flex-col gap-28 justify-start items-center min-h-dvh mx-7">
-      <Content />
+      <Suspense>
+        <Content />
+      </Suspense>
     </main>
   );
 }
